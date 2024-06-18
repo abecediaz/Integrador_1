8768b922bfa4a6abbb630bd2465d6f42bc4b7bb9
abecediaz <abecediaz@gmail.com>
2024-06-18 04:36:11 -0300

    Desarrollo de la clase Conexion y prueba de conexión con la base de datos.

--- BlockbusterManager/src/main/java/Principal/Conexion.java
+++ BlockbusterManager/src/main/java/Principal/Conexion.java
@@ -1,4 +1,22 @@
 package Principal;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Conexion {
+    static String url = "jdbc:mysql://127.0.0.1:3306/blockbuster";
+    static String user = "root";
+    static String password = "";
+    static Connection conexion = null;
+
+    public static Connection conectar() {
+        try {
+            conexion = DriverManager.getConnection(url, user, password);
+            System.out.println("LA CONEXIÓN FUE EXITOSA");
+        }
+        catch (Exception e) {
+            System.out.println("HUBO UN ERROR EN LA CONEXIÓN");
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
